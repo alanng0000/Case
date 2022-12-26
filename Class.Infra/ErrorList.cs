bd0efbbb4708c323d06b2578ab3275ae8b5eec99
Alan <terraplatform10@gmail.com>
2022-12-26 16:30:57 +0800

    Infra Error List

--- Class.Infra/ErrorList.cs
+++ Class.Infra/ErrorList.cs
@@ -5,16 +5,4 @@ namespace Class.Infra;
 
 public class ErrorList : List
 {
-    public override Error Get(object key)
-    {
-        return (Error)base.Get(key);
-    }
-
-
-
-
-    public bool Set(object key, Error value)
-    {
-        return base.Set(key, value);
-    }
 }
