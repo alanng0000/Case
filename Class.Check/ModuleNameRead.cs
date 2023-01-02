a53bf9c64ae73f2bb432c4c7cbb0556cc0eab806
Alan <terraplatform10@gmail.com>
2023-01-02 16:52:13 +0800

    Check

---
+++ Class.Check/ModuleNameRead.cs
@@ -0,0 +1,26 @@
+namespace Class.Check;
+
+
+
+
+
+
+class ModuleNameRead : Object
+{
+    public Text Text { get; set; }
+
+
+
+    public ModuleNameMap Result { get; set; }
+
+
+
+
+    public bool Execute()
+    {
+
+
+
+        return true;
+    }
+}
