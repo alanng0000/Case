90dc9d7ae89e4a1e8e1115cad4312993b701ae7d
Alan <terraplatform10@gmail.com>
2023-01-12 16:30:35 +0800

    Infra

--- Class.Infra/Import.cs
+++ Class.Infra/Import.cs
@@ -1,6 +1,8 @@
 global using Object = System.Infra.Object;
 global using Range = System.Infra.Range;
 global using RangeInfra = System.Infra.RangeInfra;
+global using Compare = System.Infra.Compare;
+global using StringCompare = System.Infra.StringCompare;
 global using List = System.List.List;
 global using Array = System.List.Array;
 global using Text = System.Text.Text;
