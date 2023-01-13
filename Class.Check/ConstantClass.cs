51dcbba9557f2a676a2bc3d9d5e94ae261098dcd
Alan <terraplatform10@gmail.com>
2023-01-14 05:43:12 +0800

    Check Constant Class

--- Class.Check/ConstantClass.cs
+++ Class.Check/ConstantClass.cs
@@ -6,77 +6,15 @@ namespace Class.Check;
 
 public class ConstantClass : Object
 {
-    public Class Bool { get; private set; }
+    public Class Bool { get; set; }
 
 
 
 
-    public Class Int { get; private set; }
+    public Class Int { get; set; }
 
 
 
 
-    public Class String { get; private set; }
-
-
-
-
-
-
-    public override bool Init()
-    {
-        base.Init();
-
-        
-
-
-        this.Bool = this.CreateClass("Bool");
-
-
-
-
-        this.Int = this.CreateClass("Int");
-
-
-
-
-        this.String = this.CreateClass("String");
-
-
-
-
-        return true;
-    }
-
-
-
-
-
-
-    private Class CreateClass(string name)
-    {
-        Class c;
-
-
-        c = new Class();
-
-
-        c.Init();
-
-
-        c.Name = name;
-
-
-
-
-
-        Class ret;
-
-
-        ret = c;
-
-
-
-        return ret;
-    }
+    public Class String { get; set; }
 }
