954b6c4f6f5819c0d13d05c30004595b5d592327
Alan <terraplatform10@gmail.com>
2023-01-13 15:03:27 +0800

    Tool

--- Class.Tool.SystemModuleGen/Import.cs
+++ Class.Tool.SystemModuleGen/Import.cs
@@ -1,1 +1,5 @@
 global using Object = System.Infra.Object;
+global using Module = Class.Check.Module;
+global using ModuleRefer = Class.Check.ModuleRefer;
+global using ModuleIntent = Class.Check.ModuleIntent;
+global using ModuleVer = Class.Check.ModuleVer;
